8de2bce674c15bc424cf524186266a50b543daf0
Pawel Akonom <pawel.akonom@gmail.com>
2023-10-28 19:14:08 +0200

    Updated google-drive-mount.sh

--- usr/local/sbin/google-drive-mount.sh
+++ usr/local/sbin/google-drive-mount.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-google-drive-ocamlfuse -label pawel.akus /mnt/gdrive/pawel.akus
-google-drive-ocamlfuse -label pawel.akonom /mnt/gdrive/pawel.akonom
+
+for GOOGLE_ACOUNT in $(ls -1 $HOME/.gdfuse); do
+  google-drive-ocamlfuse -label $GOOGLE_ACOUNT /mnt/gdrive/$GOOGLE_ACOUNT
+done
